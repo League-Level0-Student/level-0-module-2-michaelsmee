0804572685df2d19273c7ec4438e65cdbd0ac1ac
League <League@adminisorsimac2.attlocal.net>
2018-09-01 13:00:18 -0700

    Today i finished dot race, did ice cream, and went halfway through roller coaster.

---
+++ src/extra/rollercoaster.java
@@ -0,0 +1,11 @@
+
+package extra;
+
+import javax.swing.JOptionPane;
+
+public class rollercoaster {
+	public static void main(String[] args) {
+JOptionPane.showInputDialog("what is your height?");
+
+}
+}
